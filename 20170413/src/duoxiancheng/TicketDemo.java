10b2b2366cd2267bee20df47d42a6157e39b7ad0
ltt19921124 <ltt19921124@163.com>
2017-04-15 12:40:45 +0800

    l

--- 20170413/src/duoxiancheng/TicketDemo.java
+++ 20170413/src/duoxiancheng/TicketDemo.java
@@ -6,6 +6,10 @@ import javax.swing.plaf.synth.SynthSpinnerUI;
  * 卖票示例：四个窗口
  *  并发，
  * 
+ * 同步的好处：解决了线程安全问题
+ * 同步的弊端：相对降低了效率
+ * 同步的前提：有多个线程并使用同一个锁 
+ * 同一个锁
  * 
  * */
 
@@ -14,14 +18,16 @@ class Ticket implements Runnable {
 	Object obj = new Object();
 	
 	public void run() {
-		synchronized (obj) {
+	
 			while(true) {
-				if(num > 0) {
-					try{Thread.sleep(500);}catch(InterruptedException e){}
-					System.out.println(Thread.currentThread().getName() + "..sale.."+num--);
+				synchronized(obj) {
+					if(num > 0) {
+						try{Thread.sleep(100);}catch(InterruptedException e){}
+						System.out.println(Thread.currentThread().getName() + "..sale.."+num--);
+					}
 				}
 			}
-		}
+		
 	}
 }
 
