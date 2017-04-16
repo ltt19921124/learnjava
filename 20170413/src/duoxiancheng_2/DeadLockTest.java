26276585872527f9650fbcddba8a17fff7ad8b2c
ltt19921124 <ltt19921124@163.com>
2017-04-16 21:49:29 +0800

    k

---
+++ 20170413/src/duoxiancheng_2/DeadLockTest.java
@@ -0,0 +1,16 @@
+package duoxiancheng_2;
+
+
+class Test {
+	private boolean flag;
+	Test(boolean flag){
+		this.flag = flag;
+	}
+	
+}
+
+public class DeadLockTest {
+	public static void main(String[] args) {
+		
+	}
+}
