b0b1b97402cd37709e54ad62ab99ce946ff344cf
ltt19921124 <ltt19921124@163.com>
2017-04-19 13:45:26 +0800

    l

--- 20170417/src/StringBuffer/StringBufferDemo.java
+++ 20170417/src/StringBuffer/StringBufferDemo.java
@@ -9,12 +9,38 @@ public class StringBufferDemo {
 		 * 
 		 * 特点：1，长度是可变
 		 * 	   2，可以存储不同类型数据
-		 * 	   3，
-		 * 
+		 * 	   3，最终要转成字符串进行使用
+		 * 	   4,可以对字符串进行修改
 		 * 
 		 * 
 		 * */
-		bufferMethodDemmo();
+		bufferMethodDemmo_2();
+		
+	}
+
+	private static void bufferMethodDemmo_2() {
+		
+		StringBuffer sb = new StringBuffer("abce");
+		
+//		sb.delete(0, sb.length());//清空缓冲区
+//		sb.replace(1, 2, "nba");//anbace
+		
+//		sb.setCharAt(2, 'q');//abqe
+		
+//		sb.setLength(5);//设置长度
+//		System.out.println(sb);
+//		System.out.println("sb:" + sb);
+//		System.out.println("len:" + sb.length());
+		System.out.println(sb.reverse());//ecba
+		
+		
+	}
+
+	public static void bufferMethodDemmo_1() {
+		StringBuffer sb = new StringBuffer("abce");
+		
+		sb.insert(2, "qq");
+		System.out.println(sb);//abqqce
 		
 	}
 
@@ -22,9 +48,14 @@ public class StringBufferDemo {
 		//创建缓冲区对象
 		StringBuffer sb = new StringBuffer();
 		
-		sb.append(4);
-		sb.append(true);
-		System.out.println(sb);//4true
+		sb.append(4).append(false).append("haha");
+		sb.insert(1,"h");
+		System.out.println(sb);
+//		System.out.println(sb);//4falsehaha
+		
+
+//		sb.append(true);
+//		System.out.println(sb);//4true
 		
 //		StringBuffer s1 = sb.append("4");
 		
